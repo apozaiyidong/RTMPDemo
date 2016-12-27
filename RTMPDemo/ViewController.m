4b09983b428a0104a724037956b566f60d2df7d1
杨国平 <yanguogping@kdnet.net>
2016-12-27 20:30:03 +0800

    Initial Commit

---
+++ RTMPDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RTMPDemo
+//
+//  Created by 国平 杨 on 16/12/27.
+//  Copyright © 2016年 国平 杨. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ RTMPDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RTMPDemo
+//
+//  Created by 国平 杨 on 16/12/27.
+//  Copyright © 2016年 国平 杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
